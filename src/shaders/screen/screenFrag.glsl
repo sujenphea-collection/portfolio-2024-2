4cf1bf2ad14fde31d75dd47b03c2bd29c7d5130b
sujenphea <sujenphea@gmail.com>
2024-08-14 12:41:45 +1200

    feat: screen - scanline

--- src/shaders/screen/screenFrag.glsl
+++ src/shaders/screen/screenFrag.glsl
@@ -16,8 +16,11 @@ void main() {
   float green = texture(u_texture, vec2(x, y)).g;
   float blue = texture(u_texture, vec2(x + colorShift, y)).b;
 
-  // apply color
   vec3 color = vec3(red, green, blue);
+
+  // get scanline
+  float scanline = sin(v_uv.y * 1000.0) * 0.01;
+  color += scanline;
   
   // set color
   gl_FragColor = vec4(color, 1.);
