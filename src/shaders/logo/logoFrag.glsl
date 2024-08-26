32fdb035da4b078839b90ec57c19bdb409efb292
sujenphea <sujenphea@gmail.com>
2024-08-27 07:23:13 +1200

    fix: logo - color

--- src/shaders/logo/logoFrag.glsl
+++ src/shaders/logo/logoFrag.glsl
@@ -24,9 +24,9 @@ void main() {
   vec3 light = vec3(0.0);
 
   vec3 viewDirection = normalize(v_pos - cameraPosition);
-  vec3 lightPos = vec3(3.0, 8.0, -6.0);
+  vec3 lightPos = vec3(0.0, 4.0, -2.5);
   vec3 lightColor = vec3(0.8, 0.8, 1.0);
-  float lightIntensity = 3.0;
+  float lightIntensity = 10.0;
   light += lightPoint(v_pos, v_normal, viewDirection, lightPos, lightColor, lightIntensity, 20.0, 0.0, 0.0);
   
   float lightRatio = min(max(0.0, (u_progress - 0.8) * 10.0), 1.0);
