d71c23e282a58c9482e4c7ece5a48f6fbf4e1f7c
sujenphea <sujenphea@gmail.com>
2024-08-28 11:26:11 +1200

    setup: font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document"
-// import { fkGroteskkMonoFont, drukWideFont } from "./_app"
+import { Head, Html, Main, NextScript } from "next/document"
+import { cn } from "../src/utils/utils"
+import { nunitoFont } from "./_app"
 
 export default function Document() {
   return (
-    <Html lang="en">
-      {/* <Html lang="en" className={`${fkGroteskkMonoFont.variable} ${drukWideFont.variable}`}> */}
+    <Html lang="en" className={cn(nunitoFont.variable)}>
       <Head />
       <body className="bg-bgColor text-contentColor">
         <Main />
