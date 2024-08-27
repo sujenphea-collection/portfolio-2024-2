b0ca02d20f09362d61c4f68ef12cdf77caabb9e9
sujenphea <sujenphea@gmail.com>
2024-08-28 11:26:20 +1200

    feat: about - introduction

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,24 @@
+import { cn } from "../src/utils/utils"
+
 export default function About() {
-  return <div />
+  return (
+    <div className="relative">
+      <div className="">
+        <div
+          className={cn("relative min-h-[100vh]", "px-[max(3.5vw,100px)]", "flex flex-col items-center justify-center")}
+        >
+          {/* description */}
+          <h4
+            className={cn(
+              "mb-[1.8rem] max-w-[40ch]",
+              "text-center text-[2.6rem] font-medium leading-[1.05] text-[#acacac] lg:text-[4rem]"
+            )}
+          >
+            I am a Creative Web Developer based in Auckland, New Zealand.
+            <br />I believe in crafting web experiences to visually and emotionally captivate and inspire users.
+          </h4>
+        </div>
+      </div>
+    </div>
+  )
 }
