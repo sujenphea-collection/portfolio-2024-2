ab7d6a9912b212fd34ccb078515793c9cb82ffff
sujenphea <sujenphea@gmail.com>
2024-08-30 17:28:26 +1200

    fix: projects - refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,8 +15,69 @@ type ComponentRef = {
 }
 
 /* -------------------------------------------------------------------------- */
+/*                                  constans                                  */
+/* -------------------------------------------------------------------------- */
+const Projects = [
+  { title: "Project 1", description: "Description", link: "https://google.com" },
+  { title: "Project 2", description: "Description", link: "https://google.com" },
+  { title: "Project 3", description: "Description", link: "https://google.com" },
+]
+
+/* -------------------------------------------------------------------------- */
 /*                                 components                                 */
 /* -------------------------------------------------------------------------- */
+const ProjectsSection = forwardRef<ComponentRef>((_, ref) => {
+  /* ---------------------------------- refs ---------------------------------- */
+  const containerRef = useRef<HTMLDivElement | null>(null)
+
+  /* -------------------------------- functions ------------------------------- */
+  const update = () => {}
+
+  /* --------------------------------- scroll --------------------------------- */
+  useLenis(() => {})
+
+  /* --------------------------------- handle --------------------------------- */
+  useImperativeHandle(ref, () => ({
+    update,
+  }))
+
+  /* ---------------------------------- main ---------------------------------- */
+  return (
+    <div ref={containerRef} id={projectsSectionId}>
+      {Projects.map((project, i) => (
+        <div
+          // eslint-disable-next-line react/no-array-index-key
+          key={i}
+          className="pb-[150vh]"
+        >
+          <div className={cn("relative min-h-[100vh]", basePadding, "flex flex-col items-center justify-center")}>
+            <div
+              className={cn("absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2", "flex flex-col items-start")}
+            >
+              {/* title */}
+              <div className="overflow-hidden">
+                <h2
+                  className={cn(
+                    "title",
+                    "mb-[1.5rem]",
+                    "whitespace-pre font-heading text-[4.25rem] font-medium leading-[100%]"
+                  )}
+                >
+                  {project.title}
+                </h2>
+              </div>
+
+              {/* description */}
+              <h4 className={cn("desc", "max-w-[40ch]", "text-[1.25rem]")}>{project.description}</h4>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+})
+ProjectsSection.displayName = "ProjectsSection"
+
 const ContactSection = forwardRef<ComponentRef>((_, ref) => {
   /* ---------------------------------- refs ---------------------------------- */
   const containerRef = useRef<HTMLDivElement | null>(null)
@@ -84,6 +145,7 @@ ContactSection.displayName = "ContactSection"
 export default function Home() {
   /* ---------------------------------- refs ---------------------------------- */
   const contactRef = useRef<ComponentRef | null>(null)
+  const projectsRef = useRef<ComponentRef | null>(null)
 
   const dateTime = useRef(performance.now())
 
@@ -99,6 +161,7 @@ export default function Home() {
       e = Math.min(e, 1 / 20)
 
       contactRef.current?.update(e)
+      projectsRef.current?.update(e)
     }
 
     window.requestAnimationFrame(loop)
@@ -115,73 +178,7 @@ export default function Home() {
         <div id={homeSectionId} className="pb-[100vh]" />
 
         {/* projects */}
-        <div id={projectsSectionId}>
-          {/* project 1 */}
-          <div className="pb-[150vh]">
-            <div className={cn("relative min-h-[100vh]", basePadding, "flex flex-col items-center justify-center")}>
-              <div
-                className={cn(
-                  "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
-                  "flex flex-col items-start"
-                )}
-              >
-                {/* title */}
-                <h2
-                  className={cn("mb-[1.5rem]", "whitespace-pre font-heading text-[4.25rem] font-medium leading-[100%]")}
-                >
-                  Project 1
-                </h2>
-
-                {/* description */}
-                <h4 className={cn("max-w-[40ch]", "text-[1.25rem]")}>Description</h4>
-              </div>
-            </div>
-          </div>
-
-          {/* project 2 */}
-          <div className="pb-[150vh]">
-            <div className={cn("relative min-h-[100vh]", basePadding, "flex flex-col items-center justify-center")}>
-              <div
-                className={cn(
-                  "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
-                  "flex flex-col items-start"
-                )}
-              >
-                {/* title */}
-                <h2
-                  className={cn("mb-[1.5rem]", "whitespace-pre font-heading text-[4.25rem] font-medium leading-[100%]")}
-                >
-                  Project 2
-                </h2>
-
-                {/* description */}
-                <h4 className={cn("max-w-[40ch]", "text-[1.25rem]")}>Description</h4>
-              </div>
-            </div>
-          </div>
-
-          {/* project 3 */}
-          <div className="pb-[150vh]">
-            <div className={cn("relative min-h-[100vh]", basePadding, "flex flex-col items-center justify-center")}>
-              <div
-                className={cn(
-                  "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
-                  "flex flex-col items-start"
-                )}
-              >
-                {/* title */}
-                <h2
-                  className={cn("mb-[1.5rem]", "whitespace-pre font-heading text-[4.25rem] font-medium leading-[100%]")}
-                >
-                  Project 3
-                </h2>
-
-                {/* description */}
-                <h4 className={cn("max-w-[40ch]", "text-[1.25rem]")}>Description</h4>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ProjectsSection ref={projectsRef} />
 
         {/* contact */}
         <ContactSection ref={contactRef} />
