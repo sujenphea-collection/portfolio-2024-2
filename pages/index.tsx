51d0ba8963b98c9640a137478ec0c11cfab38494
sujenphea <sujenphea@gmail.com>
2024-08-30 18:31:49 +1200

    feat: projects - animate hide/show

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useLenis } from "lenis/dist/lenis-react"
+import { useRouter } from "next/router"
 import { forwardRef, useEffect, useImperativeHandle, useRef } from "react"
 import { basePadding, contactSectionId, homeSectionId, projectsSectionId } from "../src/constants/uiConstants"
 import Ease from "../src/utils/ease"
@@ -27,48 +28,85 @@ const Projects = [
 /*                                 components                                 */
 /* -------------------------------------------------------------------------- */
 const ProjectsSection = forwardRef<ComponentRef>((_, ref) => {
+  const { asPath } = useRouter()
+
   /* ---------------------------------- refs ---------------------------------- */
   const containerRef = useRef<HTMLDivElement | null>(null)
 
+  const titles = useRef<{ [key: number]: HTMLElement }>({})
+  const descs = useRef<{ [key: number]: HTMLElement }>({})
+
   /* -------------------------------- functions ------------------------------- */
-  const update = () => {}
+  const update = () => {
+    Array.from(containerRef.current?.children ?? []).forEach((el, i) => {
+      const content = el as HTMLElement
+      const bounds = content.getBoundingClientRect()
+
+      const showScreenOffset = (Properties.viewportHeight - bounds.top) / Properties.viewportHeight
+      const hideScreenOffset = -bounds.bottom / Properties.viewportHeight
+
+      content.style.visibility = showScreenOffset > 0 && hideScreenOffset < 0 ? "visible" : "hidden"
+
+      // animate title
+      const titleShowRatio = MathUtils.fit(showScreenOffset, 1, 1.5, 0, 1, Ease.cubicOut)
+      const titleHideRatio = MathUtils.fit(hideScreenOffset, -0.7, -0.2, 0, 1, Ease.cubicIn)
+      titles.current[i].style.transform = `translateY(${(1 - titleShowRatio + titleHideRatio) * 100}%)`
+
+      // animate description
+      const descShowRatio = MathUtils.fit(showScreenOffset, 1.2, 1.7, 0, 1, Ease.cubicOut)
+      const descHideRatio = MathUtils.fit(hideScreenOffset, -0.5, 0.0, 0, 1, Ease.cubicIn)
+      descs.current[i].style.transform = `translateY(${(1 - descShowRatio + descHideRatio) * 100}%)`
+    })
+  }
 
   /* --------------------------------- scroll --------------------------------- */
-  useLenis(() => {})
+  useLenis(() => {
+    Array.from(containerRef.current?.children ?? []).forEach((el) => {
+      const container = el as HTMLElement
+      const content = el.children[0] as HTMLElement
+
+      const containerTop = container.getBoundingClientRect().top
+      content.style.transform = `translateY(${-containerTop}px)`
+    })
+  })
 
   /* --------------------------------- handle --------------------------------- */
   useImperativeHandle(ref, () => ({
     update,
   }))
 
+  /* --------------------------------- effects -------------------------------- */
+  useEffect(() => {
+    Array.from(containerRef.current?.children ?? []).forEach((el, i) => {
+      const title = el.getElementsByClassName("title")[0] as HTMLElement
+      titles.current[i] = title
+
+      const desc = el.getElementsByClassName("desc")[0] as HTMLElement
+      descs.current[i] = desc
+    })
+  }, [asPath])
+
   /* ---------------------------------- main ---------------------------------- */
   return (
     <div ref={containerRef} id={projectsSectionId}>
       {Projects.map((project, i) => (
-        <div
-          // eslint-disable-next-line react/no-array-index-key
-          key={i}
-          className="pb-[150vh]"
-        >
+        // eslint-disable-next-line react/no-array-index-key
+        <div key={i} className="pb-[150vh]">
           <div className={cn("relative min-h-[100vh]", basePadding, "flex flex-col items-center justify-center")}>
             <div
               className={cn("absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2", "flex flex-col items-start")}
             >
               {/* title */}
-              <div className="overflow-hidden">
-                <h2
-                  className={cn(
-                    "title",
-                    "mb-[1.5rem]",
-                    "whitespace-pre font-heading text-[4.25rem] font-medium leading-[100%]"
-                  )}
-                >
+              <div className={cn("mb-[1.5rem]", "overflow-hidden")}>
+                <h2 className={cn("title", "whitespace-pre font-heading text-[4.25rem] font-medium leading-[1.3]")}>
                   {project.title}
                 </h2>
               </div>
 
               {/* description */}
-              <h4 className={cn("desc", "max-w-[40ch]", "text-[1.25rem]")}>{project.description}</h4>
+              <div className="overflow-hidden">
+                <h4 className={cn("desc", "max-w-[40ch]", "text-[1.25rem]")}>{project.description}</h4>
+              </div>
             </div>
           </div>
         </div>
