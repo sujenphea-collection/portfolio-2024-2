f5a9441695e221679428647b534d352ce2bf6bf3
sujenphea <sujenphea@gmail.com>
2024-10-01 12:41:48 +1300

    fix: domains

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -3,7 +3,7 @@ export const Projects = [
     title: "Content Shaker",
     description:
       "An experiment with react-three-rapier and window drag movement, exploring physics-based interactions.",
-    link: "https://content-shaker.vercel.app",
+    link: "https://shaker.sujen.co",
     colorTop: "#edfbf4",
     colorBottom: "#004510",
     imageUrl: "/projects/contentShaker.png",
@@ -11,7 +11,7 @@ export const Projects = [
   {
     title: "Terrain",
     description: "Infinitely generated terrain using Perlin noise.",
-    link: "https://terrain-olive.vercel.app",
+    link: "https://terrain.sujen.co",
     colorTop: "#1a2fed",
     colorBottom: "#585a70",
     imageUrl: "/projects/terrain.png",
@@ -20,7 +20,7 @@ export const Projects = [
     title: "Gallery",
     description:
       "An endlessly rotating carousel gallery displaying a curated selection of images, offering viewers a dynamic and immersive visual experience.",
-    link: "https://galleria-navy.vercel.app",
+    link: "https://gallery.sujen.co",
     colorTop: "#909091",
     colorBottom: "#000000",
     imageUrl: "/projects/gallery.png",
@@ -29,7 +29,7 @@ export const Projects = [
     title: "Balance",
     description:
       "An experiment in simulating the behavior and dynamics of water, exploring realistic fluid motion and interactions.",
-    link: "https://libra-black.vercel.app",
+    link: "https://balance.sujen.co",
     colorTop: "#edf2fb",
     colorBottom: "#001845",
     videoUrl: "/projects/balance.mp4",
